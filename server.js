4adfad8445c7ac905b3757e4ef9dcb84bd968080
Trent_D <tdickson91@gmail.com>
2022-07-12 18:10:38 -0400

    removed 12-16

--- server.js
+++ server.js
@@ -9,11 +9,6 @@ const app = express();
 
 const mongoose = require('mongoose');
 
-// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('01-Activities')
-  ? cwd.split('/01-Activities/')[1]
-  : cwd;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
