967d2dffa709f3aea8c976a1269a55a30af8104c
Trent_D <tdickson91@gmail.com>
2022-07-16 15:32:22 -0400

    complete, mongoose not required in server.js

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const cwd = process.cwd();
-
 const PORT = process.env.port || 3001;
 const app = express();
-
-const mongoose = require('mongoose');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,6 +11,6 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
+    console.log(`API server running on port ${PORT}!`);
   });
 });
